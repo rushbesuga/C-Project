79621e22f29585085acbe0815754ebc8f1cdde42
allen <allenyu@jepun.com.tw>
2020-10-06 11:53:33 +0800

    10/6 Rabbit MQ

---
+++ RabbitMQ/Clinet/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rabbitmq;
+namespace Clinet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+                var options = new FactoryOptions()
+            {
+                HostName = "JEPUN-PC076",
+                HostPort = 5672,
+                UserName = "Allen",
+                UserPassword = "123",
+                VirtualHost = "dev"
+            };
+
+            var msg = "hello word";
+            var publisher = new Publisher(options, "log");
+            var publisher2 = new Publisher(options, "logs-serverity2");
+            publisher.Pub("mo rout");
+
+            Console.ReadLine();
+        }
+        }
+    }
+}
